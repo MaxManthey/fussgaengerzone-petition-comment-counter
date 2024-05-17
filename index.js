2dafe3c14fbcfa39e32f662ecbfd185bf8ceda25
Max Manthey <mantheymax23@gmail.com>
2024-05-17 22:54:41 +0200

    first commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PATH =
   "./abbruch-des-verkehrsversuchs-temporaere-fussgaengerzone-in-crailsheim.csv";
 
 const comments = [];
+const wordMap = {};
 let rowsCounter = 0;
 let commentsCounter = 0;
 let publicCommentsCounter = 0;
@@ -22,7 +23,24 @@ fs.createReadStream(PATH)
     }
   })
   .on("end", function() {
+    filterComments();
+    console.log(wordMap);
     console.log("Anzahl Unterschriften:", rowsCounter);
     console.log("Anzahl Kommentare:", commentsCounter);
     console.log("Anzahl öffentliche Kommentare:", publicCommentsCounter);
   });
+
+function filterComments() {
+  comments.map((comment) =>
+    comment.split(" ").forEach((element) => {
+      // Check if the word already exists in the wordCount object
+      if (wordMap.hasOwnProperty(element)) {
+        // Increment the count by 1
+        wordMap[element]++;
+      } else {
+        // Add the word with a count of 1
+        wordMap[element] = 1;
+      }
+    })
+  );
+}
