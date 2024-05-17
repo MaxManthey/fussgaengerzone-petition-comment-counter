3915bb3051673cbdac9f592d341c9d770434bf36
Max Manthey <mantheymax23@gmail.com>
2024-05-17 23:37:26 +0200

    impl for read/write

--- index.js
+++ index.js
@@ -18,29 +18,81 @@ fs.createReadStream(PATH)
       commentsCounter += 1;
       if (singleComment !== "geheim") {
         publicCommentsCounter += 1;
-        console.log(singleComment);
+        comments.push(singleComment);
+        // console.log(singleComment);
       }
     }
   })
   .on("end", function() {
     filterComments();
-    console.log(wordMap);
+    printWordMap();
+
     console.log("Anzahl Unterschriften:", rowsCounter);
     console.log("Anzahl Kommentare:", commentsCounter);
     console.log("Anzahl öffentliche Kommentare:", publicCommentsCounter);
+    console.log("Anzahl einzigartige Worte:", Object.keys(wordMap).length);
   });
 
 function filterComments() {
-  comments.map((comment) =>
-    comment.split(" ").forEach((element) => {
-      // Check if the word already exists in the wordCount object
-      if (wordMap.hasOwnProperty(element)) {
-        // Increment the count by 1
-        wordMap[element]++;
-      } else {
-        // Add the word with a count of 1
-        wordMap[element] = 1;
-      }
-    })
+  comments
+    .map((comment) => comment.replaceAll("\n", ""))
+    .map((comment) => comment.replaceAll('"', ""))
+    .map((comment) => comment.replaceAll(",", ""))
+    .map((comment) => comment.replaceAll(".", ""))
+    .map((comment) => comment.replaceAll("!", ""))
+    .map((comment) => comment.replaceAll("?", ""))
+    .map((comment) => comment.replaceAll("-", ""))
+    .map((comment) => comment.replaceAll(";", ""))
+    .map((comment) => comment.replaceAll("“", ""))
+    .map((comment) => comment.replaceAll("„", ""))
+    .map((comment) => comment.replaceAll("'", ""))
+    .map((comment) => comment.replaceAll("(", ""))
+    .map((comment) => comment.replaceAll(")", ""))
+    .map((comment) => comment.replaceAll("…", ""))
+    .map((comment) => comment.replaceAll(":", ""))
+    .map((comment) => comment.toLowerCase())
+    .map((comment) =>
+      comment.split(" ").forEach((element) => {
+        if (wordMap.hasOwnProperty(element)) {
+          wordMap[element]++;
+        } else {
+          wordMap[element] = 1;
+        }
+      })
+    );
+}
+
+function printWordMap() {
+  let sortedWordMap = Object.entries(wordMap);
+
+  sortedWordMap.sort((a, b) => b[1] - a[1]);
+
+  for (let [word, count] of sortedWordMap) {
+    console.log(`${word}: ${count}`);
+  }
+
+  let fileContent = sortedWordMap.map(([word, count]) => `${word}: ${count}`);
+
+  fileContent.unshift("");
+  fileContent.unshift("---------");
+  fileContent.unshift("");
+  fileContent.unshift(
+    "Anzahl einzigartige Worte: " + Object.keys(wordMap).length
   );
+  fileContent.unshift(
+    "Anzahl öffentliche Kommentare: " + publicCommentsCounter
+  );
+  fileContent.unshift("Anzahl Kommentare: " + commentsCounter);
+  fileContent.unshift("Anzahl Unterschriften: " + rowsCounter);
+  writeFile(fileContent.join("\n"));
+}
+
+function writeFile(fileContent) {
+  fs.writeFile("wordCount.txt", fileContent, (err) => {
+    if (err) {
+      console.error("Error writing to file", err);
+    } else {
+      console.log("File written successfully");
+    }
+  });
 }
